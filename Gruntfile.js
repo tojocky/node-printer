a26310637d397211dc8880bb14a0a4ae56be19ce
Ion Lupascu <ionlupascu@gmail.com>
2015-03-03 08:15:34 +0000

    grunt: add build-nw

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,20 @@ module.exports = function(grunt) {
                 }
             }
         },
+        "nw-gyp": {
+            ia32: {
+                command: 'rebuild',
+                options: {
+                    arch: 'ia32'
+                }
+            },
+            x64: {
+                command: 'rebuild',
+                options: {
+                    arch: 'x64'
+                }
+            }
+        },
         copy: {
             ia32: {
                 files: [{src: 'build/Release/node_printer.node', dest: 'lib/node_printer_' + process.platform + '_ia32.node'}]
@@ -26,7 +40,13 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-node-gyp');
+    grunt.loadNpmTasks('grunt-nw-gyp');
     grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build-nw-ia32', [
+            'nw-gyp:ia32',
+            'copy:ia32'
+    ]);
 
     grunt.registerTask('build-ia32', [
             'gyp:ia32',
@@ -38,8 +58,18 @@ module.exports = function(grunt) {
             'copy:x64'
     ]);
 
+    grunt.registerTask('build-nw-x64', [
+            'nw-gyp:x64',
+            'copy:x64'
+    ]);
+
     grunt.registerTask('build', [
             'build-ia32',
             'build-x64'
+    ]);
+
+    grunt.registerTask('build-nw', [
+            'build-nw-ia32',
+            'build-nw-x64'
     ]);
 }
