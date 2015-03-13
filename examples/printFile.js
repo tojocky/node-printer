f918b50ec5a8bd0fcdfc169e5f689497e0a959ae
Ion Lupascu <ionlupascu@gmail.com>
2015-03-13 00:29:56 +0000

    example: printFile added possibility to send file and printer from command params

--- examples/printFile.js
+++ examples/printFile.js
@@ -1,10 +1,12 @@
+// use: node printFile.js [filePath printerName]
 var printer = require("../lib"),
-    filename = __filename;
+    filename = process.argv[2] || __filename;
 
 console.log('try to print file: ' + filename);
 
 printer.printFile({filename:filename
 	, printer:'HP_LaserJet_Pro_MFP_M125nw' // printer name, if missing then will print to default printer
+    , printer: process.env[3],
 	, success:function(jobID){
 		console.log("sent to printer with ID: "+jobID);
 	}
