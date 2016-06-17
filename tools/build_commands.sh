920073d707021b3c7ef2c3036d4df368ced48548
Vasu Mahesh <vasu.mahesh@hotmail.com>
2016-06-17 14:22:04 +0530

    Added - tools for building

---
+++ tools/build_commands.sh
@@ -0,0 +1,12 @@
+node-gyp clean
+node-pre-gyp configure
+node-pre-gyp build package
+node-gyp configure --target=0.36.1 --arch=x64 --dist-url=https://atom.io/download/atom-shell --module_name=node_printer --module_path=../lib/binding/node_printer/
+node-gyp build --target=0.36.1 --arch=x64 --dist-url=https://atom.io/download/atom-shell --module_name=node_printer --module_path=../lib/binding/node_printer/
+node-pre-gyp package --runtime=electron --target=0.36.1 --target_arch=x64
+node-pre-gyp configure
+node-pre-gyp build package --target_arch=ia32
+node-gyp configure --target=0.36.1 --arch=ia32 --dist-url=https://atom.io/download/atom-shell --module_name=node_printer --module_path=../lib/binding/node_printer/
+node-gyp build --target=0.36.1 --arch=ia32 --dist-url=https://atom.io/download/atom-shell --module_name=node_printer --module_path=../lib/binding/node_printer/
+node-pre-gyp package --runtime=electron --target=0.36.1 --target_arch=ia32
+node-pre-gyp-github publish
