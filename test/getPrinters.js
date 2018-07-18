6b1d22937f84473bc8838f99e95f0b41b13c9a77
cyrildlt <delicourt.cyril@sfr.fr>
2018-07-18 15:18:42 +0200

    Fix test

--- test/getPrinters.js
+++ test/getPrinters.js
@@ -1,3 +1,4 @@
+const assert = require("assert");
 exports.testLoadLibrary = function(test) {
   test.doesNotThrow(function() {
     printer = require("../");
@@ -11,11 +12,14 @@ exports.testGetprinters = function(test) {
   test.done();
 };
 
-// exports.testGetDefaultPrinterName = function(test) {
-//   printer = require("../");
-//   test.equal(typeof printer.getDefaultPrinterName(), "string");
-//   test.done();
-// };
+exports.testGetDefaultPrinterName = function(test) {
+  printer = require("../");
+  assert(
+    typeof printer.getDefaultPrinterName() === "undefined" ||
+      typeof printer.getDefaultPrinterName() === "string"
+  );
+  test.done();
+};
 
 exports.testGetSupportedJobCommands = function(test) {
   printer = require("../");
